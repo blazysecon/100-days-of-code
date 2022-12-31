cf446f6692b3a01fc4cbca19c511dcb024a98609
Giedrius Blazys <888236+blazysecon@users.noreply.github.com>
2022-12-31 12:41:15 +0200

    add pre-commit-config

---
+++ 100_days_of_code/life_in_weeks.py
@@ -0,0 +1,9 @@
+age_now = input("What is your age? ")
+for i in range(40):
+    # print(f"{i:>2}", end="")
+    for j in range(52):
+        if i < int(age_now):
+            print("\u25A6 ", end="")  # \u25FC \u25FB \u25A5 \u25A4 \u25A3
+        else:
+            print("\u25A1 ", end="")
+    print()
